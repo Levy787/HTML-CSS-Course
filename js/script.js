5a17aaff4783e5173ed841ecd9640ac4f899fe34
levy_quilli <lquilliam@deloitte.com.au>
2022-06-24 08:07:42 +1000

    Basic JS added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,69 @@
+console.log("Hello World!");
+
+const myName = "Jack Dunn";
+const h1 = document.querySelector(".heading-primary");
+// h1.textContent = myName;
+
+// JS adds an inline style
+// h1.style.backgroundColor = "red";
+
+// Event listners
+
+// h1.addEventListener("click", function () {
+//   const h1 = document.querySelector(".heading-primary");
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+// });
+
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// Mobile Nav
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Smooth Scrolling
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") window.scrollTo({ top: 0, behaviour: "smooth" });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+//Sticky nav
+const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.querySelector("body").classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.querySelector("body").classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
